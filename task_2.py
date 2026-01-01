e916a7136ad3c47fd00adcd7769d99fbbb0d6b7e
macrovion <macrovion@gmail.com>
2026-01-01 23:48:05 +0200

    Implement Koch curve with turtle graphics
    Add Koch curve drawing using recursion with turtle graphics.

--- task_2.py
+++ task_2.py
@@ -1,1 +1,22 @@
+import turtle
 
+depth = int(input("Введіть глибину рекурсії: "))
+
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+def koch_side(n):
+    if n==0:
+        t.forward(50)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_side(n-1)
+            t.left(angle)
+
+koch_side(depth)
+
+for _ in range(3):
+    t.forward(50)
+    t.right(120)
+
+screen.mainloop()
